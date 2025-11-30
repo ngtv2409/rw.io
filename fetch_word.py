cbcd78f8931f8ac38c8d9d92e7f16b34e42071ad
ngtv <tuanvy860@gmail.com>
2025-11-30 08:15:20 +0700

    update word gen

--- fetch_word.py
+++ fetch_word.py
@@ -1,37 +1,30 @@
 from nltk.corpus import wordnet as wn
 import random
 
-# Get synsets
-noun = list(wn.all_synsets(wn.NOUN))
-verb = list(wn.all_synsets(wn.VERB))
-adj = list(wn.all_synsets(wn.ADJ))
-adv = list(wn.all_synsets(wn.ADV))
+# Get lemmas
+words = [word for word in list(wn.all_lemma_names())
+    if word.isalpha() and word[0].islower() and len(word) >= 3
+]
 
-posmap = {
-    "noun": noun,
-    "verb": verb,
-    "adj": adj,
-    "adv": adv
-}
-
-def getWordRandom(pos, n = 1):
-    synsets = posmap[pos].copy()
-    random.shuffle(synsets)
+def getWordRandom(n = 1):
+    lemmas = random.sample(words, n)
     i = 0
     ret = list()
-    for synset in synsets:
-        lemmas = synset.lemmas()
-        if not lemmas:
+    for lemma in lemmas:
+        word = lemma.replace("_", " ")
+        synsets = wn.synsets(lemma)
+        if not synsets:
             continue
-        # use first lemma 
-        word = lemmas[0].name().replace("_", " ")
-        # filter words that matter
-        if word.isalpha() and word[0].islower() and len(word) >= 3:
-            defi = synset.definition()
-            i += 1
-            ret.append(
-                (word, defi, pos)
-            )
-            if i >= n:
-                break
+        defi = [(
+            synset.pos(),
+            synset.definition(), 
+            synset.examples(),
+            [l.name() for l in synset.lemmas()]
+        ) for synset in synsets if synset is not None]
+        i += 1
+        ret.append(
+            (word, defi)
+        )
+        if i >= n:
+            break
     return ret
